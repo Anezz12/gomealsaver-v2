c542cae8598f045ad7f2f869107cc950b552a06d
Anezz12 <harsenaargrtya1@gmail.com>
2025-03-14 22:36:09 +0700

    Add type annotation for meal parameter in MealsPage component

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -67,7 +67,7 @@ export default async function MealsPage() {
             </div>
           ) : (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
-              {serializedMeals.map((meal) => (
+              {serializedMeals.map((meal: any) => (
                 <MealsPageRender key={meal._id} meal={meal} />
               ))}
             </div>
