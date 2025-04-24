92594eb45391d247435dfbaf0b072fec72e0c32f
Anezz12 <harsenaargrtya1@gmail.com>
2025-04-24 23:20:18 +0700

    feat: add OrdersDashboard and OrdersPage components for order management functionality

---
+++ app/(without-navigaton)/dashboard-seller/orders/page.tsx
@@ -0,0 +1,9 @@
+import OrdersPage from '@/components/Dashboard/OrderDashboard';
+import MainContainerDashboard from '@/components/Dashboard/MainContainerDashboard';
+export default function OrdersDashboard() {
+  return (
+    <MainContainerDashboard>
+      <OrdersPage />
+    </MainContainerDashboard>
+  );
+}
