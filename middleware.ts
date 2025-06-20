e2b1e848de8fb12cabfa0e29fb418363b0af16bb
Lanang38 <cokde38@gmail.com>
2025-06-20 13:49:53 +0700

    fix: update middleware to include dashboard-seller path and correct login check

--- middleware.ts
+++ middleware.ts
@@ -12,7 +12,8 @@ export async function middleware(request: NextRequest) {
   if (session) {
     if (
       request.nextUrl.pathname === '/register' ||
-      request.nextUrl.pathname === '/login'
+      request.nextUrl.pathname === '/login' ||
+      request
     ) {
       return NextResponse.redirect(new URL('/', request.url));
     }
@@ -24,5 +25,5 @@ export async function middleware(request: NextRequest) {
 
 // Konfigurasi jalur yang perlu di-cek oleh middleware
 export const config = {
-  matcher: ['/register', '/login'],
+  matcher: ['/register', '/login', '/dashboard-seller/:path*'],
 };
